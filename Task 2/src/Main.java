a83bff779ac123641222820bb81be7112386c83f
ДимаПрибытков <d.pribytkov93@mail.ru>
2024-01-05 20:16:14 +0300

    finish method

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,22 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+      public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+       print();
+        }
+
+    private static void print() {
+        System.out.println(inputInteger() % 2 == 0);
+    }
+    private static int inputInteger() {
+        System.out.println("Введите целое число:");
+        return new Scanner (System. in).nextInt();
     }
 }
+
+
